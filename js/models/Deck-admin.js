986cfbd7777e2339bc41adf15cb36a6f21a68d43
treehouse-robertpm <robert.manolsi@teamtreehouse.com>
2020-02-09 14:48:47 -0800

    updated

---
+++ js/models/Deck-admin.js
@@ -0,0 +1,9 @@
+/**
+ * EZ Hold 'em - Deck-admin Class
+ * Developed by: Robert Manolis
+ *               Milwaukie, OR - 2019
+ */
+
+"use strict";
+
+class DeckAdmin {};
---
+++ js/models/Play-admin.js
@@ -0,0 +1,9 @@
+/**
+ * EZ Hold 'em - Play-admin Class
+ * Developed by: Robert Manolis
+ *               Milwaukie, OR - 2019
+ */
+
+"use strict";
+
+class PlayAdmin {};
